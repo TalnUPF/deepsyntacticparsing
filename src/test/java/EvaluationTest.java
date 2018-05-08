75ed566200ed58e678f07e916026c8afaf818a9f
lpmayos <lpmayos@gmail.com>
2018-05-08 16:18:01 +0200

    commented out metrics that we are not using

--- src/test/java/EvaluationTest.java
+++ src/test/java/EvaluationTest.java
@@ -34,22 +34,22 @@ public class EvaluationTest {
         
     }
 
-    @Test
-    public void equalFilesTest() throws Exception {
-        // Test: predicted and gold are the same tree
-
-        Path goldPath = Paths.get(this.deepDatasetsPath + "/EN_Deep/gold_ud2.1_ud-treebanks-v2.1_UD_English_en-ud-test.conll_out.conll");
-        String parserName = "C2L2_2017-05-13-07-09-39";
-        Path candidatePath = Paths.get(this.deepDatasetsPath + "/EN_Deep/candidate_conll2017-test-runs-v3_conll17-ud-test-2017-05-09_C2L2_2017-05-13-07-09-39_output_en.conll_out.conll");
-        
-        String id0Str = "id1="; 
-        
-        Evaluation e = new Evaluation(goldPath, candidatePath, id0Str);
-        Map<String, Double> results1 = e.HyperNodeAccuracy();
-        Map<String, Double> results2 = e.nodeLabelAndAttachment();
-        
-//        assertEquals(0.0, distance, 0.0);
-    }
+//    @Test
+//    public void equalFilesTest() throws Exception {
+//        // Test: predicted and gold are the same tree
+//
+//        Path goldPath = Paths.get(this.deepDatasetsPath + "/EN_Deep/gold_ud2.1_ud-treebanks-v2.1_UD_English_en-ud-test.conll_out.conll");
+//        String parserName = "C2L2_2017-05-13-07-09-39";
+//        Path candidatePath = Paths.get(this.deepDatasetsPath + "/EN_Deep/candidate_conll2017-test-runs-v3_conll17-ud-test-2017-05-09_C2L2_2017-05-13-07-09-39_output_en.conll_out.conll");
+//        
+//        String id0Str = "id1="; 
+//        
+//        Evaluation e = new Evaluation(goldPath, candidatePath, id0Str);
+//        Map<String, Double> results1 = e.HyperNodeAccuracy();
+//        Map<String, Double> results2 = e.nodeLabelAndAttachment();
+//        
+////        assertEquals(0.0, distance, 0.0);
+//    }
 
     private void addDeepEvaluation(String goldPathStr, String surfaceEvalResultsPathStr, String candidateStringFormat, String newResultsPath) throws Exception {
         // For each predicted surface conll file looks for the deep conll file
@@ -73,66 +73,66 @@ public class EvaluationTest {
             
             try {
                 Evaluation e = new Evaluation(goldPath, candidatePath, id0Str);
-                Map<String, Double> results1 = e.HyperNodeAccuracy();
+//                Map<String, Double> results1 = e.HyperNodeAccuracy();
                 Map<String, Double> results2 = e.nodeLabelAndAttachment();
 
                 JSONObject jsonMetrics = (JSONObject) jsonPrediction.get("metrics");
 
-                Map<String, Double> precision = new HashMap<>();
-                precision.put("average", results1.get("precision"));
-                jsonMetrics.put("deep_precision", precision);
-
-                Map<String, Double> recall = new HashMap<>();
-                recall.put("average", results1.get("recall"));
-                jsonMetrics.put("deep_recall", recall);
-
-                Map<String, Double> f1 = new HashMap<>();
-                f1.put("average", results1.get("f1"));
-                jsonMetrics.put("deep_f1", f1);
-
-                Map<String, Double> nodesDetected = new HashMap<>();
-                nodesDetected.put("average", results1.get("nodes_detected_by_system"));
-                jsonMetrics.put("deep_nodes_detected_by_system", nodesDetected);
-
-                Map<String, Double> nodesGs = new HashMap<>();
-                nodesGs.put("average", results1.get("nodes_in_gold_standard"));
-                jsonMetrics.put("deep_nodes_in_gold_standard", nodesGs);
-
-                Map<String, Double> nodesCorrectlyDetected = new HashMap<>();
-                nodesCorrectlyDetected.put("average", results1.get("nodes_correctly_detected"));
-                jsonMetrics.put("deep_nodes_correctly_detected", nodesCorrectlyDetected);            
-
-                Map<String, Double> preLas = new HashMap<>();
-                preLas.put("average", results2.get("precision_las"));
-                jsonMetrics.put("deep_precision_las", preLas); 
-
-                Map<String, Double> preUas = new HashMap<>();
-                preUas.put("average", results2.get("precision_uas"));
-                jsonMetrics.put("deep_precision_uas", preUas); 
-
-                Map<String, Double> preLA = new HashMap<>();
-                preLA.put("average", results2.get("precision_la"));
-                jsonMetrics.put("deep_precision_la", preLA); 
+//                Map<String, Double> precision = new HashMap<>();
+//                precision.put("average", results1.get("precision"));
+//                jsonMetrics.put("deep_precision", precision);
+//
+//                Map<String, Double> recall = new HashMap<>();
+//                recall.put("average", results1.get("recall"));
+//                jsonMetrics.put("deep_recall", recall);
+//
+//                Map<String, Double> f1 = new HashMap<>();
+//                f1.put("average", results1.get("f1"));
+//                jsonMetrics.put("deep_f1", f1);
+//
+//                Map<String, Double> nodesDetected = new HashMap<>();
+//                nodesDetected.put("average", results1.get("nodes_detected_by_system"));
+//                jsonMetrics.put("deep_nodes_detected_by_system", nodesDetected);
+//
+//                Map<String, Double> nodesGs = new HashMap<>();
+//                nodesGs.put("average", results1.get("nodes_in_gold_standard"));
+//                jsonMetrics.put("deep_nodes_in_gold_standard", nodesGs);
+//
+//                Map<String, Double> nodesCorrectlyDetected = new HashMap<>();
+//                nodesCorrectlyDetected.put("average", results1.get("nodes_correctly_detected"));
+//                jsonMetrics.put("deep_nodes_correctly_detected", nodesCorrectlyDetected);            
+//
+//                Map<String, Double> preLas = new HashMap<>();
+//                preLas.put("average", results2.get("precision_las"));
+//                jsonMetrics.put("deep_precision_las", preLas); 
+//
+//                Map<String, Double> preUas = new HashMap<>();
+//                preUas.put("average", results2.get("precision_uas"));
+//                jsonMetrics.put("deep_precision_uas", preUas); 
+//
+//                Map<String, Double> preLA = new HashMap<>();
+//                preLA.put("average", results2.get("precision_la"));
+//                jsonMetrics.put("deep_precision_la", preLA); 
 
                 Map<String, Double> recLas = new HashMap<>();
                 recLas.put("average", results2.get("recall_las"));
                 jsonMetrics.put("deep_recall_las", recLas); 
 
-                Map<String, Double> recUas = new HashMap<>();
-                recUas.put("average", results2.get("recall_uas"));
-                jsonMetrics.put("deep_recall_uas", recUas); 
-
-                Map<String, Double> recLA = new HashMap<>();
-                recLA.put("average", results2.get("recall_la"));
-                jsonMetrics.put("deep_recall_la", recLA); 
-
-                Map<String, Double> ucm = new HashMap<>();
-                ucm.put("average", results2.get("ucm"));
-                jsonMetrics.put("deep_ucm", ucm); 
-
-                Map<String, Double> lcm = new HashMap<>();
-                lcm.put("average", results2.get("lcm"));
-                jsonMetrics.put("deep_lcm", lcm); 
+//                Map<String, Double> recUas = new HashMap<>();
+//                recUas.put("average", results2.get("recall_uas"));
+//                jsonMetrics.put("deep_recall_uas", recUas); 
+//
+//                Map<String, Double> recLA = new HashMap<>();
+//                recLA.put("average", results2.get("recall_la"));
+//                jsonMetrics.put("deep_recall_la", recLA); 
+//
+//                Map<String, Double> ucm = new HashMap<>();
+//                ucm.put("average", results2.get("ucm"));
+//                jsonMetrics.put("deep_ucm", ucm); 
+//
+//                Map<String, Double> lcm = new HashMap<>();
+//                lcm.put("average", results2.get("lcm"));
+//                jsonMetrics.put("deep_lcm", lcm); 
             } catch (Exception ex) {
                 System.out.println("[WARNING] Parser in " + parserPath + " could not be evaluated.");
             }
